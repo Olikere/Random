25ad431d48440cd582139547b853bf283eb74f3f
Olikere <139827079+Olikere@users.noreply.github.com>
2025-08-16 17:44:35 -0300

    Create while_incress.c

---
+++ C/while_incress.c
@@ -0,0 +1,22 @@
+/*
+ 
+  simples while loop
+ 
+ */
+
+# include <stdio.h>
+
+int main (){
+
+  int counter = 1;
+
+
+  while (counter < 10 ){
+
+
+    printf("o número da vez é: %i\n",counter);
+    counter++;
+
+  }
+  return 0;
+}
