4d4145ba87110f60e0467e64e4253779dad954b4
Olikere <139827079+Olikere@users.noreply.github.com>
2025-08-16 17:53:54 -0300

    Create for_incress.c

---
+++ C/for_incress.c
@@ -0,0 +1,19 @@
+/*
+
+   simples loop for
+
+*/
+#include <stdio.h>
+
+int counter;
+
+int main (){
+
+  for(counter = 1; counter < 10; counter++){
+    printf("o número da vez é: %i\n", counter);
+
+  }
+
+  return 0;
+
+}
